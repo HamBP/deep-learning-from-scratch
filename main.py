debe1aa477ee926b310bbe4e3d715615d0d8a796
HamBP <sjy_9484@naver.com>
2022-01-24 06:00:43 +0900

    퍼셉트론

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 1
+import numpy as np
 
-    if x1 * w1 + x2 * w2 > theta:
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+
+    temp = np.sum(w * x) + b
+
+    if temp > 0:
         return 1
     else:
         return 0
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+
+    temp = np.sum(w * x) + b
+
+    if temp > 0:
+        return 1
+    else:
+        return 0
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+
+    temp = np.sum(w * x) + b
+
+    if temp > 0:
+        return 1
+    else:
+        return 0
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+

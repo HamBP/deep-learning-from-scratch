2ddded559a0f4ae24b2681cec326a1f8ab9b5a83
HamBP <sjy_9484@naver.com>
2022-02-01 23:03:18 +0900

    add learning

--- ch4/two_layer_net.py
+++ ch4/two_layer_net.py
@@ -10,7 +10,7 @@ class TwoLayerNet:
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
-        self.params['W1'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
     def predict(self, x):
@@ -37,7 +37,7 @@ class TwoLayerNet:
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
 
-    def numberical_gradient(self, x, t):
+    def numerical_gradient(self, x, t):
         loss_W = lambda W: self.loss(x, t)
 
         grads = {}
